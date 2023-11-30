43fdc0a99e7cf0837dea40a41a5f3d05d11c58e2
Vedansh Bhatt <vedansh.bhatt01@gmail.com>
2023-11-29 20:48:22 -0500

    Update main.py
    Drawing commands in appropriate function; circle drawn with a series of dots rather than countless lines.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """
 Perfect Circle Game!
 Author: Vedansh Bhatt
-Last Modified: 2023-11-24
+Last Modified: 2023-11-26
+Changes Made: Circle drawn with many points rather than a continuous line. 
 Next Steps: Put the drawing commands into the draw function.
 """
 import sys, pygame
@@ -14,11 +15,16 @@ pygame.display.set_caption("Draw Perfect Circle!")
 
 point_colour = (255, 255, 255)
 line_colour = point_colour
-start_pos, end_pos = None, None
 line_width = 2
 
 def draw():
-    pass
+    start_pos, end_pos = None, None
+    if pygame.mouse.get_pressed()[0]:
+        start_pos = event.pos
+        end_pos = event.pos
+        
+    if start_pos is not None and end_pos is not None:
+        pygame.draw.line(screen, line_colour, start_pos, end_pos, line_width)
 
 confirm = False
 
@@ -30,17 +36,17 @@ while True:
         elif event.type == pygame.MOUSEBUTTONDOWN: # for detecting first instance - user commits here
             if event.button == 1:
                 confirm = True
-                start_pos = event.pos
-                end_pos = event.pos
         elif event.type == pygame.MOUSEMOTION: # can only fire after the go-ahead from mousebuttondown
             if confirm == True:
                 draw()
-            if pygame.mouse.get_pressed()[0]:
-                end_pos = event.pos
-                
-        if start_pos is not None and end_pos is not None:
-            pygame.draw.line(screen, line_colour, start_pos, end_pos, line_width)
-        
+                # line_start = (width // 2, height // 2)
+                # line_end = (width // 2, height // 2)
+                # line_color = (255, 0, 0)
+                # line_width = 2
+                # mouse_x, mouse_y = pygame.mouse.get_pos()
+                # line_end = (mouse_x, mouse_y)
+                # pygame.draw.line(screen, line_colour, line_start, line_end, 2)
+                        
         pygame.draw.circle(screen, point_colour, (width / 2, height / 2), 5)
         
     pygame.display.flip()
